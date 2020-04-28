96c1e191770a9c14b28e6492685e223c6c8171d3
ougabriel <34310658+ougabriel@users.noreply.github.com>
2020-04-29 00:35:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
     echo "<h1>Welcome!</h1>";
+    $name = "Php Store";
+    $credit = 1000;
+    echo "<h1>Welcome to ".$name."! </h1>";
+    echo "<h2>You have $".$credit." in your wallet.</h2>";";
     ?>
   </body>
 </html>
